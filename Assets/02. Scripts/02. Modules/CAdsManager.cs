12012fdd276a43e62d02c9b327ac6cd6247e4b02
tradmin <tr.really@gmail.com>
2017-02-16 19:11:02 +0900

    Add all file

---
+++ Assets/02. Scripts/02. Modules/CAdsManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using GoogleMobileAds.Api;
+
+public class CAdsManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		RequestBanner ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void RequestBanner()
+	{
+		string strAdsID;
+
+		#if UNITY_ANDROID
+		strAdsID = "ca-app-pub-4926797886000556/9832993624";
+		#elif UNITY_IPHONE
+		strAdsID = "ca-app-pub-4926797886000556/9832993624";
+		#endif
+
+		BannerView bannerView = new BannerView (strAdsID, AdSize.Banner, AdPosition.Bottom);
+
+		AdRequest request = new AdRequest.Builder ().Build ();
+
+		bannerView.LoadAd (request);
+	}
+}
